b5534b5e3f66cfda1b2cdb0237d18179310f95d3
steven Yabann <stevekiptoo1@gmail.com>
2023-03-14 15:47:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+// Write your code here//
+let num1 = 31
+let num2 = 2
+let multiply = num1*num2 
 
-// Write your code here
+
+const num3 = 24
+const num4 = 10
+const mod = num3 % num4
+
+const max = Math.max(11,14,16,20)
+
+const random = 4
